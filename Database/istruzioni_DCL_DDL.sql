155a8533c0fb603a2d21c08210408c20c3084aeb
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2025-01-23 09:51:57 +0100

    agg

---
+++ Database/istruzioni_DCL_DDL.sql
@@ -0,0 +1,14 @@
+DROP SCHEMA fondamenti;
+DROP DATABASE fondamenti;
+
+CREATE SCHEMA fondamenti;
+CREATE DATABASE fondamenti;
+
+-- database esercitazioni
+CREATE DATABASE esercitazioni;
+GRANT ALL ON esercitazioni.* TO 'ITS_2025'@'localhost';
+
+-- database pokemon
+CREATE DATABASE pokemon;
+GRANT ALL ON pokemon.* TO 'ITS_2025'@'localhost';
+
