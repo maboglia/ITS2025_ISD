5ba04b0145161b1e6ca83f6e2bdf3107330c199e
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2025-03-13 17:55:54 +0100

    agg

---
+++ Java/eclipse-workspace/Prj07_LaboratorioInformatico/src/scarabocchi/Tabelline.java
@@ -0,0 +1,36 @@
+package scarabocchi;
+
+
+
+
+public class Tabelline {
+
+	public static void main(String[] args) {
+		
+		final int RIGHE = 10, COLONNE = 10;
+		
+		
+		for (int i = 1; i <= RIGHE; i++) {
+			
+			for (int j = 1; j <= COLONNE; j++) {
+				
+				System.out.print( i * j + "\t");
+				
+			}
+			
+			System.out.println();
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
